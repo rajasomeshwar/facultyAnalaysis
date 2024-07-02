70f2f540d571d0c31dd71b5f1fbf72fb750cf104
Rajasomeshwar <rajasomeshwarbitla@gmail.com>
2024-07-02 23:11:08 +0530

    final

--- loginout-app/src/Faculty/Facultyanalysis/ApiClinent/clientApi.js
+++ loginout-app/src/Faculty/Facultyanalysis/ApiClinent/clientApi.js
@@ -1,2 +1,5 @@
 import axios from "axios";
-export const apiClient = axios.create({ baseURL: "http://localhost:8089" });
+export const apiClient = axios.create({
+  baseURL: "https://ravishing-perfection-production.up.railway.app/",
+});
+// export const apiClient = axios.create({ baseURL: "http://localhost:8089" });
