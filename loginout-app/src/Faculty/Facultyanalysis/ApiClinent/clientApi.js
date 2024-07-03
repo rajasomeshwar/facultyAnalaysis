6f28731aa5b362db7bfa428864453f0809a67556
Rajasomeshwar <rajasomeshwarbitla@gmail.com>
2024-07-03 17:15:37 +0530

    fianle1

--- loginout-app/src/Faculty/Facultyanalysis/ApiClinent/clientApi.js
+++ loginout-app/src/Faculty/Facultyanalysis/ApiClinent/clientApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const apiClient = axios.create({
-  baseURL: "https://pleasant-ambition-production.up.railway.app/",
+  baseURL: "https://pleasant-ambition-production.up.railway.app",
 });
 // export const apiClient = axios.create({ baseURL: "http://localhost:8089" });
